1c20c653652c1ddf6852b80596554a331271116a
Naman Kumar <parnamnkh1@gmail.com>
2024-11-13 16:36:50 +0530

    uploading

---
+++ jsFoundation/part3/test.js
@@ -0,0 +1,34 @@
+/* 
+1. Declare an array named `teaFlavors` that contains the strings `"green tea"`, `"black tea"`, and `"oolong tea"`. 
+   Access the first element of the array and store it in a variable named `firstTea`.
+*/
+//  let  teaFlavors =["green tea","black tea","oolong tea"];
+//  let firstTea = teaFlavors[0]
+//  console.log(firstTea) // prints: green tea
+ 
+// /* 
+// 2. Declare an array named `cities` containing `"London"`, `"Tokyo"`, `"Paris"`, and `"New York"`. 
+//    Access the third element in the array and store it in a variable named `favoriteCity`.
+// */
+// let cities =["London", "Tokyo", "Paris", "New York"]
+// cities[2]= "jharkhand"
+// let favoriteCity = cities[2]
+//     console.log(favoriteCity) // prints: Paris
+// const originalArray = [1, 2, 3, 4, 5];
+
+// const copiedPortion = originalArray.slice(1, 4); // Start at index 1, end before index 4
+//  originalArray.pop();
+// console.log(copiedPortion); // Output: [2, 3, 4]
+// console.log(originalArray); // Output: [1, 2, 3, 4, 5] 
+
+let cities = ["Paris", "New York", "Tokyo", "London"];
+let cityList = [];
+
+for (let c = 0; c < cities.length; c++) {
+  if(cities[c]==="Paris"){
+   
+
+  }
+    
+}
+console.log(cities);
