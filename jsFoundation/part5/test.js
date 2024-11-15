1c8f9401049595da1be500e1cc58a68c6568c4cd
Naman Kumar <parnamnkh1@gmail.com>
2024-11-15 06:22:45 +0530

    advanced function over

--- jsFoundation/part5/test.js
+++ jsFoundation/part5/test.js
@@ -4,4 +4,32 @@ function order(teaType){
     }
     return confirmaion();
 }
-console.log(order("lemon tea"));
+//console.log(order("lemon tea"));
+
+// arrow function -3
+const calculateTotal = (price, quantity) => {
+    return price * quantity;
+
+}
+
+const totalCost = calculateTotal(222,32);
+//console.log(totalCost);
+ 
+// function as a para meter or higher order function -04
+
+const makeTea =(tea) =>{
+    return `the tea is ${tea}`
+}
+const proccessTea =(typeTea) =>{
+    return typeTea("lemon tea");
+}
+//console.log(proccessTea(makeTea));
+//SPECil feature of function that allows the value of the code to hunted
+
+const createTeaMaker = (flav)=> {
+    return function(teaType){
+        return `making ${teaType} ${flav}`;
+    }
+}
+const teamaker = createTeaMaker("light");
+console.log(teamaker("lemon tea"));
