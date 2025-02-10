fa70af963adc9aee1bb28f9e2b8073493d0a28e8
Naman Kumar <parnamnkh1@gmail.com>
2025-02-10 07:49:19 +0530

    completed p1

--- js-projects/01_todo-localstorage/script.js
+++ js-projects/01_todo-localstorage/script.js
@@ -1,33 +1,49 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    const input=document.getElementById("todo-input");
-    const task=document.getElementById("add-task-btn");
-    const listInput=document.getElementById("todo-list");
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.getElementById("todo-input");
+  const taskbtn = document.getElementById("add-task-btn");
+  const listInput = document.getElementById("todo-list");
 
-    let tasks = JSON.parse(localStorage.getItem(task)) || [];
-    tasks.forEach(element => rendertask(element));
-        
-    
-    task.addEventListener("click",()=>{
-        const taskText=input.value.trim();
-        if(taskText ===""){
-            alert("Please enter a task");
-        }
-        const newTask={
-            id:  Date.now(),
-            text: taskText,
-            completed: false,
-        }
-        tasks.push(newTask);
-        savetasks();
-        input.value="";
-        console.log(tasks);
-    
-    })
-    function rendertask(){
-        console.log("task");
+  let tasks = JSON.parse(localStorage.getItem("task")) || [];
+  tasks.forEach((element) => rendertask(element));
 
+  taskbtn.addEventListener("click", () => {
+    const taskText = input.value.trim();
+    if (taskText === "") {
+      alert("Please enter a task");
     }
-    function savetasks(){
-        localStorage.setItem("task",JSON.stringify(tasks));
-    }
-})
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
+    tasks.push(newTask);
+    savetasks();
+    input.value = "";
+    console.log(tasks);
+  });
+  function rendertask(element) {
+    const elementItem = document.createElement("li");
+    elementItem.setAttribute("data-id", element.id);
+    elementItem.innerHTML = `<span>${element.text}</span>
+        <button class="delete">Delete</button>`;
+    elementItem.addEventListener("click", (e) => {
+      if (e.target.tagName === "BUTTON") return;
+      element.completed = !element.completed;
+
+      elementItem.classList.toggle("completed");
+
+      savetasks();
+    });
+    elementItem.querySelector("button").addEventListener("click", (e) => {
+      e.stopPropagation();
+      tasks = tasks.filter((f) => f.id !== element.id);
+      elementItem.remove();
+      savetasks();
+    });
+
+    listInput.appendChild(elementItem);
+  }
+  function savetasks() {
+    localStorage.setItem("task", JSON.stringify(tasks));
+  }
+});
