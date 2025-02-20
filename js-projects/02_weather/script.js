493e84788053a25d9a1e8f96bfb9eb15b2229ba4
Naman Kumar <parnamnkh1@gmail.com>
2025-02-21 02:28:35 +0530

    p2 complete

--- js-projects/02_weather/script.js
+++ js-projects/02_weather/script.js
@@ -1,58 +1,48 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const cityInput = document.getElementById("city-input");
-  const getWeatherBtn = document.getElementById("get-weather-btn");
-  const weatherInfo = document.getElementById("weather-info");
-  const cityNameDisplay = document.getElementById("city-name");
-  const temperatureDisplay = document.getElementById("temperature");
-  const descriptionDisplay = document.getElementById("description");
-  const errorMessage = document.getElementById("error-messag");
+document.addEventListener('DOMContentLoaded', () => {
+    const cityInfo =document.getElementById('city-input');
+    const button=document.getElementById('get-weather-btn');
+    const weatherinfo=document.getElementById('weather-info');
+    const cityDis=document.getElementById('city-name');
+    const tempDis=document.getElementById('temperature');
+    const descriptionDis=document.getElementById('description');
+    const error=document.getElementById('error-messag');
+    const API_KEY ="8f6be32cbf15d2046d5bd1f6d17d0959";
 
-  const API_KEY = "5f56d525d1619d0a2cd2eac4ce55588e"; //env variables
+    button.addEventListener('click', async () => {
+        const city =cityInfo.value.trim();
+        if(!city)
+            return;
+        try{
+            const data = await getWeather(city);
+          displayWeather(data);
+            
+        }
+        catch(err){
+         
+            showError();
+        }
 
-  getWeatherBtn.addEventListener("click", async () => {
-    const city = cityInput.value.trim();
-    if (!city) return;
+    });
 
-    // it may throw an error
-    // server/database is always in another continent
-
-    try {
-      const weatherData = await fetchWeatherData(city);
-      displayWeatherData(weatherData);
-    } catch (error) {
-      showError();
+    async function getWeather(city){
+        const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+        const response =await fetch(url);
+        if (!response.ok) {
+            throw new Error(" City Not found");
+          }
+        const data =await response.json();
+        console.log(data);
+        return data;
     }
-  });
-
-  async function fetchWeatherData(city) {
-    //gets the data
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
-
-    const response = await fetch(url);
-    console.log(typeof response);
-    console.log("RESPONSE", response);
-
-    if (!response.ok) {
-      throw new Error(" City Not found");
+    function displayWeather(data){
+        cityDis.textContent = data.name;
+        tempDis.textContent = data.main.temp;
+        descriptionDis.textContent = data.weather[0].description;
+        weatherinfo.classList.remove("hidden");
+        error.classList.add("hidden");
     }
-    const data = await response.json();
-    return data;
-  }
-
-  function displayWeatherData(data) {
-    console.log(data);
-    const { name, main, weather } = data;
-    cityNameDisplay.textContent = name;
-    temperatureDisplay.textContent = `Temperature : ${main.temp}`;
-    descriptionDisplay.textContent = `Weather : ${weather[0].description}`;
-
-    //unlock the display
-    weatherInfo.classList.remove("hidden");
-    errorMessage.classList.add("hidden");
-  }
-
-  function showError() {
-    weatherInfo.classList.remove("hidden");
-    errorMessage.classList.add("hidden");
-  }
+    function showError() {
+        weatherinfo.classList.remove("hidden");
+        errorMessage.classList.add("hidden");
+      }
 });
