ecaa4fe84a896a2f523cec2281aee7110d328d53
Naman Kumar <parnamnkh1@gmail.com>
2025-03-20 03:11:37 +0530

    file fs handeling

--- nodejs/todo/todo.js
+++ nodejs/todo/todo.js
@@ -1,42 +1,41 @@
+
 const fs = require("fs");
-const filePath = "./tasks.json";
+const path = "./tasks.json"
 
-const loadTasks = () => {
-  try {
-    const dataBuffer = fs.readFileSync(filePath);
-    const dataJSON = dataBuffer.toString();
-    return JSON.parse(dataJSON);
-  } catch (error) {
-    return [];
-  }
+const command= process.argv[2];
+const argument= process.argv[3];
+
+const addTask = (arg) => {
+const tasks = loadTasks();
+tasks.push(arg);
+saveTask(tasks);
+console.log("task added" + arg);
+
 };
-
-const saveTasks = (tasks) => {
-  const dataJSON = JSON.stringify(tasks);
-  fs.writeFileSync(filePath, dataJSON);
-};
-
-const addTask = (task) => {
-  const tasks = loadTasks();
-  tasks.push({ task });
-  saveTasks(tasks);
-  console.log("Task addded ", task);
-};
-
-const listTasks = () => {
-  const tasks = loadTasks();
-  tasks.forEach((task, index) => console.log(`${index + 1} - ${task.task}`));
-};
-
-const command = process.argv[2];
-const argument = process.argv[3];
-
-if (command === "add") {
-  addTask(argument);
-} else if (command === "list") {
-  listTasks();
-} else if (command === "remove") {
-  removeTask(parseInt(argument));
-} else {
-  console.log("Command not found !");
+function saveTask(tasks){
+    const data = JSON.stringify(tasks);
+    fs.writeFileSync(path,data);
 }
+const loadTasks = ()=>{
+    try {
+        const dataBuffer = fs.readFileSync(path);
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+      } catch (error) {
+        return [];
+      }
+};
+const listTasks = () => {
+    const tasks = loadTasks();
+    tasks.forEach((task, index) => console.log(`${index + 1} - ${task.task}`));
+  };
+  
+if (command === "add") {
+    addTask(argument);
+  } else if (command === "list") {
+    listTasks();
+  } else if (command === "remove") {
+    removeTask(parseInt(argument));
+  } else {
+    console.log("Command not found !");
+  }
